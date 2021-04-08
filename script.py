ca05aed44ad315fd21f9e8606ca93cc522712095
Brian Mann <mannb986@gmail.com>
2021-04-08 19:39:34 +0200

    added loop to print titels and lowered cased all titles and authors

--- script.py
+++ script.py
@@ -2,3 +2,9 @@ import utils
 import sorts
 
 bookshelf = utils.load_books('books_small.csv')
+
+# This prints a list of all titles in the bookshelf
+for book in bookshelf:
+    print(book['title_lower'])
+    
+
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import csv
+import os
+
 
 # This code loads the current book
 # shelf data from the csv file
@@ -7,7 +9,7 @@ def load_books(filename):
   with open(filename) as file:
       shelf = csv.DictReader(file)
       for book in shelf:
-          # add your code here
-          
+          book['author_lower'] = book['author'].lower()
+          book['title_lower'] = book['title'].lower()
           bookshelf.append(book)
   return bookshelf
