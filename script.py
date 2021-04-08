d75debb4814252fcab4172f77b895c4c60040cc1
Brian Mann <mannb986@gmail.com>
2021-04-08 20:30:47 +0200

    code completed and tested successfully

--- script.py
+++ script.py
@@ -2,10 +2,7 @@ import utils
 import sorts
 
 bookshelf = utils.load_books('books_small.csv')
-
-# This prints a list of all titles in the bookshelf
-# for book in bookshelf:
-#     print(book['title_lower'])
+long_bookshelf = utils.load_books('books_large.csv')
 
 bookshelf_v1 = bookshelf.copy()
 bookshelf_v2 = bookshelf.copy()
@@ -23,7 +20,12 @@ def by_author_ascending(book_a, book_b):
     else:
         return False
 
-# def by_total_length()
+def by_total_length(book_a, book_b):
+    if len(book_a["author_lower"]) + len(book_a['title_lower']) > len(book_b['author_lower']) + len(['title_lower']):
+        return True
+    else:
+        return False
+
 
 # Running the different sort alogrithms with different comparison functions
 sort_1 = sorts.bubble_sort(bookshelf, by_title_ascending)
@@ -38,4 +40,6 @@ sorts.quicksort(bookshelf_v2, 0, len(bookshelf_v2)-1, by_author_ascending)
 for book in bookshelf_v2:
     print(book['author_lower'])
 
-
+sorts.quicksort(long_bookshelf, 0, len(long_bookshelf)-1, by_total_length)
+for book in long_bookshelf:
+    print(book['title'])
